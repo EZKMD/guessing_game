d404876e7c2f603244dd253e900ddedd7707e09a
EZKMD <emidhun92@gmail.com>
2025-06-26 13:09:57 +0930

    Removed secret number message

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,7 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::rng().random_range(1..=100);
-
-    println!("The secret number is: {secret_number}");
+    let secret_number = rand::rng().random_range(1..=100);    
 
     loop {
         println!("Please input your guess.");
